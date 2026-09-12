3a724f6f6347b08408c6e2f1b5132881d40fe2c4
BhavanaSrinivas94 <bhavanasrinivas94@gmail.com>
2026-09-12 20:52:19 +0530

    DAY 1: Setup student task manager UI

---
+++ script.js
@@ -0,0 +1,7 @@
+console.log("Student Task Manager started");
+
+const addTaskButton = document.getElementById("addTaskButton");
+
+addTaskButton.addEventListener("click", function () {
+    alert("Task creation will be added on Day 2!");
+});
